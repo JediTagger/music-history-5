5530a2b9402f8d19401de91f93acd684e1e19deb
Jesse Turner <jesseturner@comcast.net>
2015-08-10 16:46:13 -0500

    functional filter, clear filter, submit, and sumit another buttons

--- javascripts/main.js
+++ javascripts/main.js
@@ -20,73 +20,87 @@ requirejs(
   function($, fb, Handlebars, bootstrap, dom, pop) {
   	//this assigns the arguments to the above dependencies
     var myFirebaseRef = new Firebase("https://flickering-inferno-4277.firebaseio.com/");
-      myFirebaseRef.on("value", function(snapshot) {
+    myFirebaseRef.on("value", function(snapshot) {
 
-      	var showSongs = function(data) {
-          require(['hbs!../templates/songs'], function(songTemplate) {
-            dom.html(songTemplate(data));
-          });
-      	};
-        showSongs(snapshot.val());
+    	var showSongs = function(data) {
+        require(['hbs!../templates/songs'], function(songTemplate) {
+          dom.html(songTemplate(data));
+        });
+    	};
+      showSongs(snapshot.val());
 
-        var showArtists = function(data) {
-          require(['hbs!../templates/artists'], function(artistTemplate) {
-            $('#artists').append(artistTemplate(data));
-          });
-        };
-        showArtists(snapshot.val());
+      var showArtists = function(data) {
+        require(['hbs!../templates/artists'], function(artistTemplate) {
+          $('#artists').append(artistTemplate(data));
+        });
+      };
+      showArtists(snapshot.val());
 
-        var showAlbums = function(data) {
-          require(['hbs!../templates/albums'], function(albumTemplate) {
-            $('#albums').append(albumTemplate(data));
-          });
-        };
-        showAlbums(snapshot.val());
+      var showAlbums = function(data) {
+        require(['hbs!../templates/albums'], function(albumTemplate) {
+          $('#albums').append(albumTemplate(data));
+        });
+      };
+      showAlbums(snapshot.val());
+      
+      var artistSelection = "";
+      var albumSelection = "";
 
-        console.log(snapshot.val().songs[0].artist);
-
-        $('#filter').click(function() {
-        var filteredSongs = [];
-        for(artistSelection in snapshot.val()) {
-          if (artistSelection === snapshot.val().songs[0].artist) {
-            filteredSongs.push(snapshot.val().songs[0]);
-          };
-        };
-        showSongs(filteredSongs);
+      $(document).on("click", "#artists li a", function() {
+        artistSelection = $(this).text();
       });
+
+      $(document).on("click", "#albums li a", function() {
+        albumSelection = $(this).text();
+      });
+
+      $('#filter').click(function() {
+        var filteredSongs = {};
+        filteredSongs.songs = {};
+        for(var i in snapshot.val().songs) {
+          if (artistSelection === snapshot.val().songs[i].artist) {
+            filteredSongs.songs[i] = snapshot.val().songs[i];
+          }
+        }
+        showSongs(filteredSongs);
+        $('.clear').show();
+      });
+
+      $('#clear').click(function() {
+        showSongs(snapshot.val());
+        $('.clear').hide();
+      });
+
     });
 
   $(document).on("click", "#submit", function() {
-    var newSong = {
-      "name": $('#newSong').val(),
-      "artist": $('#newArtist').val(),
-      "album": $('#newAlbum').val()
-    };
 
-    $.ajax({
-     url: "https://flickering-inferno-4277.firebaseio.com/songs.json",
-     method: "POST",
-     data: JSON.stringify(newSong)
-    }).done(function(newSong) {
-      console.log("post initiated");
-    });
+    if($('#newSong').val() === "" || $('#newArtist').val() === "" || $('#newAlbum').val() === "") {
+
+      $('.incomplete').show();
+
+    }else{
+      var newSong = {
+        "name": $('#newSong').val(),
+        "artist": $('#newArtist').val(),
+        "album": $('#newAlbum').val()
+      };
+
+      $.ajax({
+        url: "https://flickering-inferno-4277.firebaseio.com/songs.json",
+        method: "POST",
+        data: JSON.stringify(newSong)
+      }).done(function(newSong) {
+        $('.success').show();
+        $('.incomplete').hide();
+        $('#submit').hide();
+        $('#another').show();
+      });
+    }
   });
 
-  $(document).on("click", "#artists li a", function() {
-    var artistSelection = $(this).text();
-  });
-
-  $(document).on("click", "#albums li a", function() {
-    var albumSelection = $(this).text();
+  $(document).on("click", "#another", function() {
+    location.reload(true);
   });
 
 });
-
-    //obsolete remove and more buttons
-    // $(document).on("click", "#rmv", function() {
-    //  $(this).parent().remove();
-
-    // dom.append("<button id=\"more\">More ></button>");
-
-    // $(document).on("click", "#more", function() {
-    //  more.moreSongs(showSongs);
